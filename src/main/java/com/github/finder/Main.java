ebd201aed457fc5878fd186afa9a22dabb3aed2b
yuge shunki <g1345503@cc.kyoto-su.ac.jp>
2015-07-03 17:30:28 +0900

    refs #12 Mainに，Finderオブジェクトのfindを呼び出し，結果を出力する処理を追加する．
    Main のコンストラクタで次の処理を追加する．
    
    Finder クラスのオブジェクトを作成する．
    Args オブジェクトの iterator() メソッドに対して繰り返し処理を行う．
    各要素に対して，次の処理を行う．
    Finder オブジェクトに対して，find を実行する．
    返り値として得られた文字列配列について，繰り返す．
    要素ひとつひとつを出力する（System.out.println を利用する）．

--- src/main/java/com/github/finder/Main.java
+++ src/main/java/com/github/finder/Main.java
@@ -1,7 +1,21 @@
 package com.github.finder;
 
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.CmdLineException;
+
 public class Main{
     public Main(String[] arguments){
+        Args args = parseArguments(arguments);
+	Finder finder = new Finder(args);
+        for(String base: args){
+            String[] items = finder.find(base);
+            for(String item: items){
+                System.out.println(item);
+            }
+        }
+    }
+    
+    private Args parseArguments(String[] arguments){
         Args args = new Args();
         try {
             CmdLineParser parser = new CmdLineParser(args);
@@ -10,7 +24,7 @@ public class Main{
         }
         return args;
     }
-
+    
     public static void main(String[] args){
         new Main(args);
     }
